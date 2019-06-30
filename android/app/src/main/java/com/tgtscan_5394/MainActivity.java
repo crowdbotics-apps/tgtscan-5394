b3788e8d95f0046bfbd80763875d4650c14de594
Crowdbotics <team@crowdbotics.com>
2019-06-30 11:03:19 +0000

    initial commit

---
+++ android/app/src/main/java/com/tgtscan_5394/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tgtscan_5394;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "tgtscan_5394";
+    }
+}
